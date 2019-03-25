698caa45215522ceda68682607cc22da327509b6
Jamil Rabbii <32033651+jamil-rabbii@users.noreply.github.com>
2019-03-26 01:31:42 +0600

    your commentt

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+
+
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-      // Schema::defaultStringLength(191);
+      
     }
 
     /**
@@ -23,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-     //  Schema::defaultStringLength(191);
+      Schema::defaultStringLength(191);
     }
 }
---
+++ resources/views/admin/login/login.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Log In</title>
+        <link rel = "stylesheet" type = "text/css" href="{{asset('admin')}}/css/style.css" />
+		<link rel="stylesheet" type="text/css" href="{{asset('admin')}}/css/font-awesome.min.css" media="all" />
+		 <link rel="shortcut icon" href="{{asset('admin')}}/images/favicon/TheShahibiens.jpg" type="image/x-icon"/>
+    </head>
+    <body>
+		<div class="background_log fix">
+			<div class="user_log fix">
+				<h3>Log In</h3>
+				 <form method="POST" action="{{ route('login') }}">
+                      @csrf					
+                    <input type = "text" name="email" placeholder = " User name or Email" required />
+				   <input type = "password" name="password" placeholder = "Password" required />
+					<input type="submit" valu="login">
+                                    {{ __('Login') }}
+                                </input>
+
+                            
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    </a>
+					<p><a href="{{asset('register')}}">Create Account</a> <a href="">Forget Password</a></p>
+				</form>
+			</div>
+		</div>
+    </body>
+</html>
